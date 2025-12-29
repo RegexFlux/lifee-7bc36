5667b6dd076da1726458bf7e52c68a6770df0199
ThomasTourrette <thomas.tourrette@codebusters.fr>
2025-12-29 18:36:57 +0100

    savfe

--- lifee/src/lib/s3/presignGet.ts
+++ lifee/src/lib/s3/presignGet.ts
@@ -1,9 +1,10 @@
 // src/lib/s3/index.ts
 import {GetObjectCommand} from "@aws-sdk/client-s3";
 import {getSignedUrl} from "@aws-sdk/s3-request-presigner";
-import {S3_BUCKET_NAME, s3Client, signExpires} from "@/lib/s3/client";
 
 export async function presignGetObject(params: { key: string; expiresIn?: number }) {
+    const {s3Client, S3_BUCKET_NAME, signExpires} = await import("./client"); // import dynamique
+
     const cmd = new GetObjectCommand({
         Bucket: S3_BUCKET_NAME,
         Key: params.key,
